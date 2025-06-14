4fa3e6306c0f9e56386006b1c2755ea64e50ddb3
Ghina Fadiyah Hr <gfadiyahhr@gmail.com>
2025-06-15 01:32:25 +0700

    membuat tampilan nama mobil

--- app/src/main/java/com/ghinafadiyahhr/ghinafadiyah_607062300001_asses3mobpro/screen/MainScreen.kt
+++ app/src/main/java/com/ghinafadiyahhr/ghinafadiyah_607062300001_asses3mobpro/screen/MainScreen.kt
@@ -1,7 +1,9 @@
 package com.ghinafadiyahhr.ghinafadiyah_607062300001_asses3mobpro.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,13 +18,17 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -67,7 +73,8 @@ fun ScreenContent(modifier: Modifier = Modifier) {
 @Composable
 fun ListItem(mobil: Mobil) {
     Box(
-        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray)
+        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray),
+        contentAlignment = Alignment.BottomCenter
     ){
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
@@ -78,6 +85,23 @@ fun ListItem(mobil: Mobil) {
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxWidth().padding(4.dp)
         )
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(4.dp)
+                .background(Color(red = 0f, green = 0f, blue = 0f, alpha = 0.5f))
+                .padding(4.dp)
+        ){
+            Text(
+                text = mobil.nama,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Text(
+                text = mobil.deskripsi,
+                fontStyle = FontStyle.Italic,
+                fontSize = 14.sp,
+                color = Color.White
+            )
+        }
     }
 }
 
